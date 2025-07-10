09848151e7a2c1cfe4e093c8a75c0913d2809ee2
unknown <cbolandross@gmail.com>
2025-07-10 22:13:00 +0100

    Fixes logical error making universe const infinity instead of empty

--- interval.h
+++ interval.h
@@ -31,6 +31,6 @@ class interval
 };
 
 const interval interval::empty = interval(+infinity, -infinity);
-const interval interval::universe = interval(+infinity, -infinity);
+const interval interval::universe = interval(-infinity, +infinity);
 
 #endif
