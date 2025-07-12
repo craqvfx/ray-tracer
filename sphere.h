a652e2f1aa35edfc1a60540d8022886ab2591cb9
Christian Boland Ross <151463542+craqvfx@users.noreply.github.com>
2025-07-12 16:48:26 +0100

    Updates hittable.h to record the material of a hit, and sphere.h to implement this.

--- sphere.h
+++ sphere.h
@@ -8,7 +8,10 @@
 class sphere : public hittable
 {
     public:
-        sphere(const point3& center, double radius) : center(center), radius(std::fmax(0, radius)) {}
+        sphere(const point3& center, double radius) : center(center), radius(std::fmax(0, radius)) 
+        {
+            // TODO: Initialize the material pointer `mat`.
+        }
 
         bool hit(const ray&r, interval ray_t, hit_record& rec) const override
         {
@@ -37,6 +40,7 @@ class sphere : public hittable
             rec.p = r.at(rec.t);
             vec3 outward_normal = (rec.p - center) / radius;
             rec.set_face_normal(r, outward_normal);
+            rec.mat = mat;
             
             return true;
         }
@@ -44,6 +48,7 @@ class sphere : public hittable
     private:
         point3 center;
         double radius;
+        shared_ptr<material> mat;
 };
 
 #endif
