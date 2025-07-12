356a142bcc4a3d1d59b6ffb715075c06c3d60dce
Christian Boland Ross <151463542+craqvfx@users.noreply.github.com>
2025-07-12 18:03:53 +0100

    Adds a metal material and relevant procedures

--- vec3.h
+++ vec3.h
@@ -150,4 +150,9 @@ inline vec3 random_on_hemisphere(const vec3& normal)
         return -on_unit_sphere;
 }
 
+inline vec3 reflect(const vec3& v, const vec3& n)
+{
+    return v - 2*dot(v,n)*n;
+}
+
 #endif // End if statement started at top by #ifndef VEC3_H.
